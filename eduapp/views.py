25022a7b8e6c9046184cd2872223fbf0fc74420a
thunder-007 <v.harshavardhan3@gmail.com>
2022-08-09 18:44:39 +0530

    search engine post :boom:

--- eduapp/views.py
+++ eduapp/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,HttpResponse
+import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import json
 # Create your views here.
 
 def index(request):
-    return render(request,"eduapp/index.html")
+    if request.method == 'POST':
+        search_query = request.POST
+        print(search_query['search_query'])
+        return HttpResponse("Post request")
+    return render(request, 'eduapp/index.html')
 
 
 def react(request):
