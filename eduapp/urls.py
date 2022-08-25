6c551b419d4073f5df71245b1d71334681021d60
thunder-007 <v.harshavardhan3@gmail.com>
2022-08-25 11:20:21 +0530

    query url changed

--- eduapp/urls.py
+++ eduapp/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.index, name='index'),
     path('player/<str:video_url>', views.player, name='player'),
-    path('<str:search_query>', views.results, name='results'),
+    path('results/<str:search_query>', views.results, name='results'),
     path('api/<str:search_query>',views.apiResults,name='api')
 ]
 
